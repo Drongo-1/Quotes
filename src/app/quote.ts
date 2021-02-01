80bae8b5ca6a5710aa5dfea67cc8e37c9d22f4c2
Drongo-1 <ngugidavid46@gmail.com>
2021-02-01 18:01:21 +0000

    added styles

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,14 @@
 export class Quote {
     public showMeta:boolean;
-    constructor(public quoteID:number, public message:string, public author:string, public upVote:number, public downVote:number, public dateAdded:Date, public postedBy:string ){
+    constructor(
+        public quoteID:number,
+        public message:string, 
+        public author:string, 
+        public upVote:number, 
+        public downVote:number, 
+        public dateAdded:Date,
+        public postedBy:string )
+        {
         this.showMeta = false
     }
 }
